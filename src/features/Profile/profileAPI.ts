3f41972013fefa1bc0a48800bcef42e8cb0137e4
OlgaKapskaya <olikbuko@gmail.com>
2023-01-07 01:40:38 +0300

    feat(profileAPI): added changeUserName()

---
+++ src/features/Profile/profileAPI.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+export const instance = axios.create({
+  baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+  withCredentials: true,
+})
+
+export const profileAPI = {
+  changeUserName(name: string) {
+    return instance.put('/auth/me', { name })
+  },
+}
