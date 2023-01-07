cdad55bc88cf857c6713c5a2410659ffbf3d0edc
OlgaKapskaya <olikbuko@gmail.com>
2023-01-07 23:28:36 +0300

    feat(profileAPI): added types

--- src/features/Profile/profileAPI.ts
+++ src/features/Profile/profileAPI.ts
@@ -1,12 +1,20 @@
 import axios from 'axios'
 
+import { UserInfoType } from './profileSlice'
+
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
   withCredentials: true,
 })
 
+export type ProfileResponseType = {
+  updateUser: UserInfoType
+  token: string
+  tokenDeathTime: number
+}
+
 export const profileAPI = {
-  changeUserName(name: string) {
-    return instance.put('/auth/me', { name })
+  changeUserData(name: string) {
+    return instance.put<ProfileResponseType>('/auth/me', { name })
   },
 }
