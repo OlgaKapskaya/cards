02aab7341b1ffd79e89a35aa73220dba6df49cac
tepek92 <tepek92@mail.ru>
2023-01-08 03:38:57 +0700

    build(Login): style and structure

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -2,11 +2,11 @@ import { FC, useState, MouseEvent } from 'react'
 
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Checkbox from '@mui/material/Checkbox'
 import FormControl from '@mui/material/FormControl'
-import FormControlLabel from '@mui/material/FormControlLabel'
 import IconButton from '@mui/material/IconButton'
 import Input from '@mui/material/Input'
 import InputAdornment from '@mui/material/InputAdornment'
@@ -15,6 +15,26 @@ import Paper from '@mui/material/Paper'
 import TextField from '@mui/material/TextField'
 
 import s from './Login.module.css'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat',
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Montserrat';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 500;
+          src: url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600&display=swap');
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
+})
 
 export const Login: FC = () => {
   const [showPassword, setShowPassword] = useState(false)
@@ -25,58 +45,63 @@ export const Login: FC = () => {
 
   return (
     <div>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          fontFamily: 'Montserrat, serif',
-          '& > :not(style)': {
-            m: 1,
-            width: 413,
-            height: 552,
-            margin: '50px auto',
-          },
-        }}
-      >
-        <Paper elevation={3}>
-          <div className={s.paper_container}>
-            <div className={s.title}>Sign in</div>
-            <TextField
-              className={s.email}
-              sx={{ m: 1, width: '347px', fontFamily: 'Montserrat, serif' }}
-              id="email"
-              label="Email"
-              variant="standard"
-            />
-            <FormControl className={s.password} sx={{ m: 1, width: '347px' }} variant="standard">
-              <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-              <Input
-                id="standard-adornment-password"
-                type={showPassword ? 'text' : 'password'}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+              m: 1,
+              width: 413,
+              height: 552,
+              margin: '50px auto',
+            },
+          }}
+        >
+          <Paper elevation={3}>
+            <div className={s.paper_container}>
+              <div className={s.title}>Sign in</div>
+              <TextField
+                className={s.email}
+                sx={{ m: 1, width: '347px' }}
+                id="email"
+                label="Email"
+                variant="standard"
               />
-            </FormControl>
-            <FormControlLabel
-              control={<Checkbox className={s.checkbox} defaultChecked />}
-              label="Remember me"
-            />
-            <div>Forgot Password?</div>
-            <Button variant="contained">Sign In</Button>
-            <div>Already have an account?</div>
-            <a href="#?">Sing Up</a>
-          </div>
-        </Paper>
-      </Box>
+              <FormControl className={s.password} sx={{ m: 1, width: '347px' }} variant="standard">
+                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+                <Input
+                  id="standard-adornment-password"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+              <div className={s.checkbox}>
+                <Checkbox defaultChecked />
+                <span>Remember me</span>
+              </div>
+
+              <div className={s.forgot}>Forgot Password?</div>
+              <Button className={s.btn} variant="contained">
+                Sign In
+              </Button>
+              <div className={s.already}>Already have an account?</div>
+              <a href="#?">Sing Up</a>
+            </div>
+          </Paper>
+        </Box>
+      </ThemeProvider>
     </div>
   )
 }
