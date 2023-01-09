a445ce6f6766b790bf7e76358018e68a5a5906dc
kirkade <mookj@mail.ru>
2023-01-09 11:01:30 +0300

    feat(reg): add input validation

--- src/features/Registration/Registration.tsx
+++ src/features/Registration/Registration.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { yupResolver } from '@hookform/resolvers/yup'
 import { Visibility, VisibilityOff } from '@mui/icons-material'
 import { IconButton, InputAdornment, InputLabel } from '@mui/material'
 import Button from '@mui/material/Button'
@@ -21,25 +22,32 @@ type IFormInput = {
   confirmPassword: string
 }
 
-const schema = yup
-  .object({
-    email: yup.string().email(),
-    password: yup.number().positive().integer().required(),
-  })
-  .required()
+const schema = yup.object({
+  email: yup.string().email(),
+  password: yup
+    .string()
+    .required('Password is required')
+    .min(4, 'Password length should be at least 4 characters')
+    .max(12, 'Password cannot exceed more than 12 characters'),
+  confirmPassword: yup
+    .string()
+    .required('Confirm Password is required')
+    .min(4, 'Password length should be at least 4 characters')
+    .max(12, 'Password cannot exceed more than 12 characters')
+    .oneOf([yup.ref('password')], 'Passwords do not match'),
+})
 
 export const Registration = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<IFormInput>()
-
-  const dispatch = useAppDispatch()
+  } = useForm<IFormInput>({ resolver: yupResolver(schema), mode: 'onTouched' })
   const onSubmit: SubmitHandler<IFormInput> = data => dispatch(signUp(data))
-  const [showPassword, setShowPassword] = React.useState(false)
-  const handleClickShowPassword = () => setShowPassword(show => !show)
+  const dispatch = useAppDispatch()
 
+  const [showPassword, setShowPassword] = useState(false)
+  const handleClickShowPassword = () => setShowPassword(show => !show)
   const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault()
   }
@@ -48,7 +56,7 @@ export const Registration = () => {
     <div className={s.container}>
       <div className={s.formBlock}>
         <h2 className={s.header}>Sign Up</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
           <FormControl sx={{ padding: 4 }}>
             <FormGroup>
               <TextField
@@ -58,7 +66,27 @@ export const Registration = () => {
                 variant="standard"
                 {...register('email')}
               />
-              {errors.email?.type === 'required' && <p role="alert">First name is required</p>}
+              {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+              <FormControl variant="standard">
+                <InputLabel htmlFor="standard-adornment-password-required">Password</InputLabel>
+                <Input
+                  id="standard-adornment-password-required"
+                  type={showPassword ? 'text' : 'password'}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  {...register('password', { required: 'Password is required!' })}
+                />
+              </FormControl>
+              {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
               <FormControl variant="standard">
                 <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
                 <Input
@@ -75,29 +103,14 @@ export const Registration = () => {
                       </IconButton>
                     </InputAdornment>
                   }
-                  {...register('password')}
+                  {...register('confirmPassword', {
+                    required: 'Please confirm password!',
+                  })}
                 />
               </FormControl>
-              <FormControl variant="standard">
-                <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-                <Input
-                  id="standard-adornment-password"
-                  type={showPassword ? 'text' : 'password'}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                      >
-                        {showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  {...register('confirmPassword')}
-                />
-              </FormControl>
-
+              {errors.confirmPassword && (
+                <p style={{ color: 'red' }}>{errors.confirmPassword.message}</p>
+              )}
               <Button type={'submit'} variant={'contained'} color={'primary'}>
                 Sign Up
               </Button>
