273ebd1fd0fc1cc9abe71f055d1df4d777822f17
OlgaKapskaya <olikbuko@gmail.com>
2023-01-12 01:21:40 +0300

    fix: refactor instance baseURL, emailMessage messageLinkURL

--- src/common/constants/emailMessage.ts
+++ src/common/constants/emailMessage.ts
@@ -3,7 +3,7 @@ export const emailRecoveryMessage = `
     <div style="background:#fff;padding: 15px 20px;max-width: 413px;border: 1px solid #e3e3e5;border-radius:3px;margin: 0 auto;font: normal 13px/19px Verdana;box-shadow: 0 3px 7px rgba(0,0,0,.1);">
         <h2 style="font:normal 21px/48px Arial;color: #222;text-align: center;">Password recovery</h2>
         <div style="text-align: center; padding-bottom: 10px;">To get a new password, follow the link below:</div>
-        <a href="http://localhost:3000/#/set-new-password/$token$" style="max-width: 400px;margin:0 auto;display: block;background: #1976d2; color: #fff;font-weight:bold; line-height: 44px;text-align: center;text-transform: uppercase;text-decoration: none;border-radius: 30px;text-shadow: 0 1px 3px rgba(0,0,0,.35);box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%)">Restore password</a>
+        <a href="http://olgakapskaya.github.io/cards/#/set-new-password/$token$" style="max-width: 400px;margin:0 auto;display: block;background: #1976d2; color: #fff;font-weight:bold; line-height: 44px;text-align: center;text-transform: uppercase;text-decoration: none;border-radius: 30px;text-shadow: 0 1px 3px rgba(0,0,0,.35);box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%), 0 1px 5px 0 rgb(0 0 0 / 12%)">Restore password</a>
         <div style="padding: 15px 0;text-align: center;">If you did not request a password, then simply delete this email. Your password is stored in a safe place and is not available to unauthorized persons.</div>
     </div>
 </div>
