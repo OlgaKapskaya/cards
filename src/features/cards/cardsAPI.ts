6237ce075a2378efc392f0fd726ed8d97f5d6f81
tepek92 <tepek92@mail.ru>
2023-01-15 22:36:58 +0700

    feat(Cards): cards api

---
+++ src/features/cards/cardsAPI.ts
@@ -0,0 +1,127 @@
+import { instance } from '../../common/constants/instance'
+
+export const cardsAPI = {
+  getCards(payload: GetCardsPayloadType) {
+    return instance.get<GetCardsResponseType>('cards/card', { params: payload })
+  },
+  createCard(payload: CreateCardPayloadType) {
+    return instance.post<CreateCardResponseType>('cards/card', payload)
+  },
+  deleteCard(payload: DeleteCardType) {
+    return instance.delete<DeleteCardResponseType>('cards/card', { params: payload })
+  },
+  updateCard(payload: UpdateCardPayloadType) {
+    return instance.put<UpdateCardResponseType>('cards/card', payload)
+  },
+}
+
+export type GetCardsPayloadType = {
+  cardsPack_id: string
+  cardAnswer?: string
+  cardQuestion?: string
+  min?: number
+  max?: number
+  sortCards?: number
+  grade?: number
+  page?: number
+  pageCount?: number
+}
+export type GetCardsResponseType = {
+  cards: CardType[]
+  cardsTotalCount: number
+  maxGrade: number
+  minGrade: number
+  page: number
+  pageCount: number
+  packUserId: string
+}
+export type CardType = {
+  _id: string
+  cardsPack_id: string
+  user_id: string
+  answer: string
+  question: string
+  grade: number
+  shots: number
+  comments: string
+  type: string
+  rating: number
+  more_id: string
+  created: string
+  updated: string
+  __v: number
+}
+
+export type CreateCardPayloadType = {
+  cardsPack_id: string
+  answer?: string
+  question?: string
+  grade?: number
+  shots?: number
+  answerImg?: string
+  questionImg?: string
+  questionVideo?: string
+  answerVideo?: string
+}
+
+export type CreateCardResponseType = {
+  newCard: CardType
+  token: string
+  tokenDeathTime: number
+}
+
+export type DeleteCardType = {
+  id: string
+}
+export type DeleteCardResponseType = {
+  deletedCard: CardType
+  token: string
+  tokenDeathTime: number
+}
+export type UpdateCardPayloadType = {
+  card: {
+    _id: string
+    cardsPack_id?: string
+    user_id?: string
+    answer?: string
+    question?: string
+    grade?: number
+    shots?: number
+    comments?: string
+    type?: string
+    rating?: number
+    more_id?: string
+    created?: string
+    updated?: string
+    __v?: number
+    answerImg?: string
+    answerVideo?: string
+    questionImg?: string
+    questionVideo?: string
+  }
+}
+export type UpdateCardResponseType = {
+  updatedCard: UpdateCardResponseTypeUpdatedCard
+  token: string
+  tokenDeathTime: number
+}
+export type UpdateCardResponseTypeUpdatedCard = {
+  _id: string
+  cardsPack_id: string
+  user_id: string
+  answer: string
+  question: string
+  grade: number
+  shots: number
+  comments: string
+  type: string
+  rating: number
+  more_id: string
+  created: string
+  updated: string
+  __v: number
+  answerImg: string
+  answerVideo: string
+  questionImg: string
+  questionVideo: string
+}
