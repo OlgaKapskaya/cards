09b6bed8ff15e6130d1ed0e3bd89ab7b0013a51a
OlgaKapskaya <olikbuko@gmail.com>
2023-01-18 13:37:24 +0300

    fix(Table): deleted unused files from project, refactor Error404

--- src/common/components/error404/Error404.tsx
+++ src/common/components/error404/Error404.tsx
@@ -1,17 +1,20 @@
-import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-import SuperButton from '../SuperButton/SuperButton'
+import { PATH } from '../../constants/path'
+import { ButtonComponent } from '../button/ButtonComponent'
 
 import error404 from './404.svg'
 import s from './Error404.module.css'
 
-export const Error404: FC = () => {
+export const Error404 = () => {
+  const navigate = useNavigate()
+
   return (
     <div className={s.mainContainer}>
       <div className={s.textContainer}>
         <h2 className={s.title}>Ooops!</h2>
         <span className={s.text}>Sorry! Page not found!</span>
-        <SuperButton> Back to home page </SuperButton>
+        <ButtonComponent onClick={() => navigate(PATH.PACKS)}>Back to home page</ButtonComponent>
       </div>
       <img src={error404} alt="404" />
     </div>
