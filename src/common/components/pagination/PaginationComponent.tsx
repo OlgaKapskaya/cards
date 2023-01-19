1c50688c5598caa9e813a9cb38c978ff94c1a389
tepek92 <tepek92@mail.ru>
2023-01-19 17:14:57 +0700

    refactor(Pagination): refactor pagination and throttle

--- src/common/components/pagination/PaginationComponent.tsx
+++ src/common/components/pagination/PaginationComponent.tsx
@@ -2,7 +2,6 @@ import { ChangeEvent, MouseEvent, FC, useEffect, useState } from 'react'
 
 import TablePagination from '@mui/material/TablePagination'
 
-import { useDebounce } from '../../hooks/useDebounce'
 import { useThrottle } from '../../hooks/useThrottle'
 
 type PaginationComponentPropsType = {
@@ -36,22 +35,6 @@ export const PaginationComponent: FC<PaginationComponentPropsType> = ({
   const onChangePageHandler = (event: MouseEvent<HTMLButtonElement> | null, page: number) => {
     setPage(page + 1)
   }
-  // const [rowsPerPage, setRowsPerPage] = useState(pageSize)
-  //
-  // useEffect(() => {
-  //   if (pageSize === rowsPerPage) return
-  //   setRowsPerPage(pageSize)
-  // }, [pageSize])
-  //
-  // const onChangeRowsPerPageHandler = (
-  //   event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  // ) => {
-  //   setRowsPerPage(parseInt(event.target.value, 10))
-  //   onPageChanged(1, parseInt(event.target.value, 10))
-  // }
-  // const onChangePageHandler = (event: MouseEvent<HTMLButtonElement> | null, page: number) => {
-  //   onPageChanged(page + 1, rowsPerPage)
-  // }
 
   return (
     <TablePagination
